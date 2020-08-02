89a980e85204764e40b22dfa542f18216180581a
Jonathan Takahashi <jonathan.takahashi@cvisionai.com>
2020-08-02 17:09:45 -0400

    Allow reinitialization of trackers

--- modules/tracking/src/tracker.cpp
+++ modules/tracking/src/tracker.cpp
@@ -55,11 +55,6 @@ Tracker::~Tracker()
 bool Tracker::init( InputArray image, const Rect2d& boundingBox )
 {
 
-  if( isInit )
-  {
-    return false;
-  }
-
   if( image.empty() )
     return false;
 
